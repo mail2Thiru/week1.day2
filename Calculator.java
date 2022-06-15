567901ebac0bdb8c1818e9aa6a094f4ddbf62d50
mail2Thiru <107543831+mail2Thiru@users.noreply.github.com>
2022-06-15 14:41:24 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+public class Calculator {
+	
+	public int sum(int num1, int num2) {
+		int sum = num1 +num2;
+		return sum;
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator cal = new Calculator();
+		int sum = cal.sum(5, 3);
+		System.out.println(sum);
+		
+	
+
+
+	}
+
+}
---
+++ Factorial.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+	int	i,F=1;
+	int a=5;
+	for(i=1;i<=a;i++)
+		F=F*i;
+		
+	System.out.println("Result:"+F);
+	}
+
+}
